ecc334b5fe25d1288568a2f399b379a52fdb8af3
eamuelle <eamuelle@umich.edu>
2017-09-12 16:52:53 -0400

    Revised test_sine.py to comment code and structure function sinewave to take in a frequency, amplitude and phase and return array of sinewave plane points.

--- test_sine.py
+++ test_sine.py
@@ -1,6 +1,19 @@
+#import necessary packages
 from mayavi import mlab
 import numpy
-x, y = numpy.mgrid[-10:10:100j, -10:10:100j]
-r = numpy.sin(x)*numpy.sin(y)
-mlab.surf(r, warp_scale='auto')
+
+#generate and return array r of sine wave plane points given a frequency, amplitude and phase
+def sinewave(freqfn, ampfn, phasefn):
+    x, y = numpy.mgrid[-10:10:100j, -10:10:100j]
+    r = ((ampfn)*numpy.sin((x*freqfn)+ phasefn))*((ampfn)*numpy.sin((y*freqfn)+ phasefn))
+    print (r)
+    return r
+    
+#random freq, amp, and phase to test sine wave plane response
+freq, amp, phase = 1.5, 1, (numpy.pi/2)
+
+#generate surf from sinewave function with frequency, amplitude, and phase
+mlab.surf(sinewave(freq, amp, phase), warp_scale='auto')
+
+#show mlab figure
 mlab.show()
